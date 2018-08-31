5f70476a8f54a3e241c7b953f2a3c2baeac31974
liemvo <liemvt@gmail.com>
2018-09-01 09:46:44 +1200

    Link view with controller

--- tictactoe/controller/ViewController.swift
+++ tictactoe/controller/ViewController.swift
@@ -10,16 +10,47 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var button00: UIButton!
+    @IBOutlet weak var button01: UIButton!
+    @IBOutlet weak var button02: UIButton!
+    @IBOutlet weak var button10: UIButton!
+    @IBOutlet weak var button11: UIButton!
+    @IBOutlet weak var button12: UIButton!
+    @IBOutlet weak var button20: UIButton!
+    @IBOutlet weak var button21: UIButton!
+    @IBOutlet weak var button22: UIButton!
+    @IBOutlet weak var resultGroupView: UIView!
+    @IBOutlet weak var playerLabel: UILabel!
+    @IBOutlet weak var resultMessage: UILabel!
+    
+    var model: Board?
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        model = Board()
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
     }
 
-
+    @IBAction func onCellTapped(_ sender: UIButton) {
+        
+    }
+    
+    @IBAction func reset(_ sender: UIBarButtonItem) {
+        resultGroupView.isHidden = true
+        playerLabel.text = ""
+        resultMessage.text = ""
+        button00.titleLabel?.text = ""
+        button01.titleLabel?.text = ""
+        button02.titleLabel?.text = ""
+        button10.titleLabel?.text = ""
+        button11.titleLabel?.text = ""
+        button12.titleLabel?.text = ""
+        button20.titleLabel?.text = ""
+        button21.titleLabel?.text = ""
+        button22.titleLabel?.text = ""
+    }
+    
 }
 
